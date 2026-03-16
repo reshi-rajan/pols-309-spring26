3b47b8fda4cfa3d0eb1687140ebda9bea8fae835
Reshi Rajan <122573204+reshi-rajan@users.noreply.github.com>
2026-03-15 20:29:44 -0500

    Week 8 Content Added

--- Week 8: Simple Regressions/Week 8 Lab Activity Answer Key.R
+++ Week 8: Simple Regressions/Week 8 Lab Activity Answer Key.R
@@ -12,10 +12,6 @@ library(tidyverse)
 ### Load the Data ###
 pres_vote <- read.csv("https://raw.githubusercontent.com/reshi-rajan/pols-309-spring26/refs/heads/main/Week%208%3A%20Simple%20Regressions/PresVote.csv")
 
-#### Covariances ####
-
-## If we have Two RVs X and Y, the variance of these two means captures the covariancec
-
 #### Correlations ####
 
 ## Correlations are statistics that explain how related two variables are to each other.
@@ -24,23 +20,33 @@ pres_vote <- read.csv("https://raw.githubusercontent.com/reshi-rajan/pols-309-sp
 ## 1. They must be between -1 and 1 (with values at |1| being strongest and those near 0 being weakest)
 ## 2. They are functions of tht variance and covariance 
 
-## In R, suppose we want to know the correlation between being a Democratic President
+## In R, suppose we want to know the correlation between being an incumbent Democratic President
 ## and the income of Americans. 
 
 ## Looking at our dataset, what do we need first?
 
-# We need a variable that captures whether a Democrat is in the incumbent in the WH.
+## How do we create this variable? 
 
-## How do we create this variable?
-
+pres_vote <- pres_vote %>%
+  mutate(Inc_Pres_Party = ifelse(year == 1948 | year == 1952 | year == 1964 |
+                                   year == 1968 | year == 1980 | year == 1996 |
+                                   year == 2000 | year == 2012, 1, 0))
 
 ## We can test the correlation of this using the following: 
 
 cor.test(pres_vote$Inc_Pres_Party, pres_vote$rdi4)
 
+# We can see here that the correlation between a Democratic incumbent and income.
+# is nearly 0, suggesting that there is not a strong association between the two
+
 ## How do we create a variable that tells us who the winner is? 
 
-#### Regressions Basics ####
+pres_vote <- pres_vote %>%
+  mutate(winner = ifelse(year == 1948 | year == 1960 | year == 1964 |
+                           year == 1976 | year == 1992 | year == 1996 |
+                           year == 2008 | year == 2012, 1, 0))
+
+#### Simple Regressions ####
 
 ## Correlations are nice because they can tell us how two variables move together,
 ## but they tell us nothing about which one causes the other to move, if at all,
@@ -58,6 +64,3 @@ cor.test(pres_vote$Inc_Pres_Party, pres_vote$rdi4)
 ## With these three assumptions, most models will give us claims about the world
 ## that are causal in nature.
 
-## Our models are normally of the form: Y = a + b_1*X + b_2*Z + b_3*W + e
-
-## What is going on here in this model? 
